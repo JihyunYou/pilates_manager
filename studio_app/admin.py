e3de2896bcad19b7bca752a4f2ddfa71a8fe82d5
2021000372PC <jhyou8912@gmail.com>
2022-04-01 09:29:10 +0900

    Update Admin Page

--- studio_app/admin.py
+++ studio_app/admin.py
@@ -1,10 +1,37 @@
 from django.contrib import admin
+from django.db.models import Q
 from import_export.admin import ImportExportMixin
+
+from common_app.models import User
 from studio_app.models import Studio, Member, Membership
 
 
+# 지점 정보 Admin Page 설정
 class StudioAdmin(ImportExportMixin, admin.ModelAdmin):
-    pass
+    # 화면에 출력되는 컬럼 리스트
+    list_display = (
+        'id', 'name', 'owner', 'address', 'get_teachers', 'created_by', 'created_at', 'updated_by', 'updated_at'
+    )
+
+    fieldsets = (
+        (
+            '기본 정보', {'fields': ('name', 'owner', 'address')}
+        ),
+        (
+            '소속 강사', {'fields': ('teachers',)}
+        ),
+        (
+            '작성자', {'fields': ('created_by', 'updated_by')}
+        )
+    )
+
+    def get_teachers(self, obj):
+        return ', '.join([teacher.name for teacher in obj.teachers.all()])
+
+    def render_change_form(self, request, context, *args, **kwargs):
+        context['adminform'].form.fields['owner'].queryset = User.objects.filter(user_type__lte=2)
+        context['adminform'].form.fields['teachers'].queryset = User.objects.filter(Q(user_type=2) | Q(user_type=3))
+        return super(StudioAdmin, self).render_change_form(request, context, *args, **kwargs)
 
 
 class MemberAdmin(ImportExportMixin, admin.ModelAdmin):
@@ -12,10 +39,24 @@ class MemberAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = (
         'id', 'name', 'studio', 'get_status_display', 'created_by', 'created_at', 'updated_by', 'updated_at'
     )
-    pass
+
+    # CHG
+    fieldsets = (
+        (
+            '기본 정보', {'fields': ('name', 'studio', 'status')}
+        ),
+        (
+            '작성자', {'fields': ('created_by', 'updated_by')}
+        )
+    )
 
 
 class MembershipAdmin(ImportExportMixin, admin.ModelAdmin):
+    # 화면에 출력되는 컬럼 리스트
+    list_display = (
+        'id', 'member', 'reg_date', 'reg_amount', 'payment_method', 'reg_seq', 'number_of_lesson', 'created_by', 'created_at', 'updated_by', 'updated_at'
+    )
+
     pass
 
 
